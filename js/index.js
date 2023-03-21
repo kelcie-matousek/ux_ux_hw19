310ebcdef1bdfbc023fb49cb0e517ef4539b30e7
kelcie-matousek <kelcie.matousek@gmail.com>
2023-03-21 16:45:27 -0500

    attempted to connect jquery

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 console.log("Your index.js file is loaded correctly!");
+$('nav li').click(function() {
+    if ( $("a").first().is(":hidden")) {
+        $("ul li a").show("slow"); 
+    } else {
+        $("ul li a").slideUp();
+    }
+    });
